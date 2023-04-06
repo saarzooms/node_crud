5519e5ad914c40bb1079f001021276d3151b1783
arzoo.shaikh <arzoo.rkcet@gmail.com>
2023-04-06 14:34:11 +0530

    Update blog.model.js

--- app/models/blog.model.js
+++ app/models/blog.model.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose')
 
+var schema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    }
+   },
+   {
+    timestamps:true
+   }
+   );
+
+   var blog = new mongoose.model('Blog', schema);
+   module.exports = blog;
