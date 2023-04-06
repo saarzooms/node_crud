511471207a847d69eaf584e16791641f0c590ee5
arzoo.shaikh <arzoo.rkcet@gmail.com>
2023-04-06 14:23:36 +0530

    db connection done

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express();
 const port = 5000;
 
+const dbConfig = require('./app/config/db.config')
+
+const mongoose = require("mongoose")
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log('connection done with database');
+}).catch((err)=>{
+    console.log("error in db connection ",err);
+    process.exit()
+})
 
 //create server on port
 app.listen(port,()=>{
